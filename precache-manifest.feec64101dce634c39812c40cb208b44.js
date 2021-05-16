fc87c03ab2cb3be06837efeed0160979a727a125
sens <sens@local>
2021-05-16 23:57:40 +0300

    change vue.config

---
+++ precache-manifest.feec64101dce634c39812c40cb208b44.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bde438b54718dc08b74",
+    "url": "kriss-drummer/css/app.4878b76b.css"
+  },
+  {
+    "revision": "04f0a04e63ec318e08d6",
+    "url": "kriss-drummer/css/chunk-vendors.fe3f57ce.css"
+  },
+  {
+    "revision": "b086f2ee725f02090ba3c149d3e99266",
+    "url": "kriss-drummer/img/kriss_image.jpg"
+  },
+  {
+    "revision": "830efcc6d35e5ea6f1263c22d4835fdc",
+    "url": "kriss-drummer/img/kriss_photo.jpg"
+  },
+  {
+    "revision": "82932ee0ab4b901cb5a386361afea010",
+    "url": "kriss-drummer/index.html"
+  },
+  {
+    "revision": "4bde438b54718dc08b74",
+    "url": "kriss-drummer/js/app.ed2050d3.js"
+  },
+  {
+    "revision": "04f0a04e63ec318e08d6",
+    "url": "kriss-drummer/js/chunk-vendors.3fcd8154.js"
+  },
+  {
+    "revision": "90a123d2072ec2abaf8866454e38d5da",
+    "url": "kriss-drummer/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "kriss-drummer/robots.txt"
+  },
+  {
+    "revision": "c3f757877748d84f31f484bfe1c3bf7d",
+    "url": "kriss-drummer/videos/kriss_video.mp4"
+  }
+]);
